338bf6416c66bf5ec275d7d31fdf967214819586
fy48 <fy48@fresnoU.org>
2023-04-13 09:40:04 -0700

    updated madlibs

--- Madlibs.js
+++ Madlibs.js
@@ -23,6 +23,8 @@ let question18 = readLine.question('Write a type of time:');
 let question19 = readLine.question('Write a verb:');
 let question20 = readLine.question('Write a noun:');
 let question21 = readLine.question('Write a verb:');
+let question22 = readLine.question('Write a verb:');
+
 
 question.push(question1,question2,question3,question4,question5,question6,question7,question8,question9,question10,question11,question12,question13,question14,question15,question16,question17,question18,
     question19, question20, question21,);
